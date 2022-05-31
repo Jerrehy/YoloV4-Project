344ee6d29e72e32b65fcd8c7e141faea5fa092ec
Jerrehy <sacha15012000@gmail.com>
2022-05-31 13:35:40 +0300

    Edit detect_video file for colab

--- detect_video.py
+++ detect_video.py
@@ -106,7 +106,7 @@ def main(_argv):
         pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
         image = utils.draw_bbox(frame, pred_bbox)
         fps = 1.0 / (time.time() - start_time)
-        print("FPS: %.2f" % fps)
+        # print("FPS: %.2f" % fps)
         result = np.asarray(image)
         # cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
